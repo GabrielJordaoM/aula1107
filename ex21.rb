6595bff3547556841d349449710fe4855ac1f544
Gabriel Jordão <54850185+GabrielJordaoM@users.noreply.github.com>
2023-07-12 19:42:59 -0300

    Update ex21.rb
    complementando o ex 21, que estava incompleto

--- ex21.rb
+++ ex21.rb
@@ -6,6 +6,14 @@ nome = gets.chomp
 
 if idade.is_a?(Integer)
     puts "olá #{nome} você tem #{idade} anos !!"
-else 
-    exit
+    
+if idade < 18 
+    puts "Menor de idade"
+elsif idade >=18 && idade < 65
+    puts "Adulto"
+elsif idade >= 65 
+    puts "idoso"
+else
+    puts "invalido"
+end
 end
